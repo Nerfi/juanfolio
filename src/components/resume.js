5665b947ee946ecdd3497b9646b2be3f50f5b3f8
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-10-07 17:36:32 +0200

    .

--- src/components/resume.js
+++ src/components/resume.js
@@ -6,9 +6,30 @@ function Resume(){
       <div>
         <Grid>
 
-        <Cell col={4} left side>left</Cell>
+        <Cell col={4} left side>
 
-        <Cell col={8} right scene>Right</Cell>
+        <div style={{textAling: 'center'}}>
+        <img
+        src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2013%2F07%2F13%2F10%2F07%2Fman-156584_640.png&f=1&nofb=1"
+        alt="avatar"
+        style={{height: '200px'}}
+
+        />
+
+
+        </div>
+
+        <h2 style={{paddingTop: '2em'}}>Juan</h2>
+        <h4 style={{color: 'grey'}}>Programmer</h4>
+        <hr style={{borderTop: '3px solid' }}/>
+
+
+        </Cell>
+
+        <Cell className="resume-right-col" col={8} right scene>Right
+
+
+        </Cell>
 
         </Grid>
       </div>
