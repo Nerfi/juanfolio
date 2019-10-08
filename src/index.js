2d91e54f86a4a820f46d5e3acf932cfce340b926
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-10-08 16:35:30 +0200

    deploying not working

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import * as serviceWorker from './serviceWorker';
